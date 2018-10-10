dfcb92d8219f41f3dcec19a1edf6e9aa0d4b880d
Troels lund <trolund@gmail.com>
2018-10-10 21:04:02 +0200

    splittet ud i klasser

--- main.py
+++ main.py
@@ -1,123 +1,7 @@
 import pygame
 import Player
 import settings
-
-class Platform(pygame.sprite.Sprite):
-    """ Platform the user can jump on """
-
-    def __init__(self, width, height):
-        """ Platform constructor. Assumes constructed with user passing in
-            an array of 5 numbers like what's defined at the top of this code.
-            """
-        super().__init__()
-
-        self.image = pygame.Surface([width, height])
-        self.image.fill(settings.GREEN)
-
-        self.rect = self.image.get_rect()
-
-
-class Level():
-    """ This is a generic super-class used to define a level.
-        Create a child class for each level with level-specific
-        info. """
-
-    def __init__(self, player):
-        """ Constructor. Pass in a handle to player. Needed for when moving
-            platforms collide with the player. """
-        self.platform_list = pygame.sprite.Group()
-        self.enemy_list = pygame.sprite.Group()
-        self.player = player
-
-        # How far this world has been scrolled left/right
-        self.world_shift = 0
-
-    # Update everythign on this level
-    def update(self):
-        """ Update everything in this level."""
-        self.platform_list.update()
-        self.enemy_list.update()
-
-    def draw(self, screen):
-        """ Draw everything on this level. """
-
-        # Draw the background
-        screen.fill(settings.BLUE)
-
-        # Draw all the sprite lists that we have
-        self.platform_list.draw(screen)
-        self.enemy_list.draw(screen)
-
-    def shift_world(self, shift_x):
-        """ When the user moves left/right and we need to scroll
-        everything: """
-
-        # Keep track of the shift amount
-        self.world_shift += shift_x
-
-        # Go through all the sprite lists and shift
-        for platform in self.platform_list:
-            platform.rect.x += shift_x
-
-        for enemy in self.enemy_list:
-            enemy.rect.x += shift_x
-
-
-# Create platforms for the level
-class Level_01(Level):
-    """ Definition for level 1. """
-
-    def __init__(self, player):
-        """ Create level 1. """
-
-        # Call the parent constructor
-        Level.__init__(self, player)
-
-        self.level_limit = -1000
-
-        # Array with width, height, x, and y of platform
-        level = [[210, 70, 500, 500],
-                 [210, 70, 800, 400],
-                 [210, 70, 1000, 500],
-                 [210, 70, 1120, 280],
-                 ]
-
-        # Go through the array above and add platforms
-        for platform in level:
-            block = Platform(platform[0], platform[1])
-            block.rect.x = platform[2]
-            block.rect.y = platform[3]
-            block.player = self.player
-            self.platform_list.add(block)
-
-
-# Create platforms for the level
-class Level_02(Level):
-    """ Definition for level 2. """
-
-    def __init__(self, player):
-        """ Create level 1. """
-
-        # Call the parent constructor
-        Level.__init__(self, player)
-
-        self.level_limit = -1000
-
-        # Array with type of platform, and x, y location of the platform.
-        level = [[210, 30, 450, 570],
-                 [210, 30, 850, 420],
-                 [210, 30, 1000, 520],
-                 [210, 30, 1120, 280],
-                 ]
-
-        # Go through the array above and add platforms
-        for platform in level:
-            block = Platform(platform[0], platform[1])
-            block.rect.x = platform[2]
-            block.rect.y = platform[3]
-            block.player = self.player
-            self.platform_list.add(block)
-
+import LevelController
 
 def main():
     """ Main Program """
@@ -134,8 +18,8 @@ def main():
 
     # Create all the levels
     level_list = []
-    level_list.append(Level_01(player))
-    level_list.append(Level_02(player))
+    level_list.append(LevelController.Level_01(player))
+    level_list.append(LevelController.Level_02(player))
 
     # Set the current level
     current_level_no = 0
