9d63dfea005d26dfcc3bca236e14bd60a1e5fd8b
trolund@gmail.com <vxh77kdp>
2018-10-26 22:06:38 +0200

    enemy colider

--- Player.py
+++ Player.py
@@ -68,6 +68,36 @@ class Player(pygame.sprite.Sprite):
             # Stop our vertical movement
             self.change_y = 0
 
+
+        ### Enemy colider ###
+
+        # See if we hit anything
+        enemy_hit_list = pygame.sprite.spritecollide(self, self.level.enemy_list, False)
+        for block in enemy_hit_list:
+            # If we are moving right,
+            # set our right side to the left side of the item we hit
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            elif self.change_x < 0:
+                # Otherwise if we are moving left, do the opposite.
+                self.rect.left = block.rect.right
+
+        # Move up/down
+        self.rect.y += self.change_y
+
+        # Check and see if we hit anything
+        enemy_hit_list = pygame.sprite.spritecollide(self, self.level.enemy_list, False)
+        for block in enemy_hit_list:
+
+            # Reset our position based on the top/bottom of the object.
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.change_y < 0:
+                self.rect.top = block.rect.bottom
+
+            # Stop our vertical movement
+            self.change_y = 0
+
     def calc_grav(self):
         """ Calculate effect of gravity. """
         if self.change_y == 0:
